fbe948d8b3a3c284ec084c0e8bc81b5285d8af33
ShiTong <ShiTong@DESKTOP-7LFVAJV>
2020-04-05 23:32:55 +0800

    update check attachment

--- src/Timor.Cms.Service/Articles/ArticleService.cs
+++ src/Timor.Cms.Service/Articles/ArticleService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using MongoDB.Bson;
@@ -7,16 +8,18 @@ using Timor.Cms.Dto.Articles.CreateArtile;
 using Timor.Cms.Dto.Articles.GetArticleById;
 using Timor.Cms.Infrastructure.Dependency;
 using Timor.Cms.Repository.MongoDb;
+using Timor.Cms.Service.Attachments;
 
 namespace Timor.Cms.Service.Articles
 {
     public class ArticleService : ITransient
     {
-        private readonly  IMongoDbRepository<Article> _articleRepository;
+        private readonly IMongoDbRepository<Article> _articleRepository;
         private readonly IMapper _mapper;
-
-        public ArticleService(IMongoDbRepository<Article> articleRepository, IMapper mapper)
+        private readonly AttachmentService _attachmentService;
+        public ArticleService(AttachmentService attachmentService, IMongoDbRepository<Article> articleRepository, IMapper mapper)
         {
+            _attachmentService = attachmentService;
             _articleRepository = articleRepository;
             _mapper = mapper;
         }
@@ -30,6 +33,17 @@ namespace Timor.Cms.Service.Articles
 
         public async Task CreateArtice(ArticeInput artice)
         {
+            var attachmentIds = artice.AttachmentIds;
+            attachmentIds.Add(artice.ImageId);
+
+            foreach (var attachmentId in attachmentIds)
+            {
+                if (await _attachmentService.CheckAttachmentExist(attachmentId))
+                {
+                    return;
+                }
+            }
+
             var entity = _mapper.Map<Article>(artice);
 
             await _articleRepository.InsertAsync(entity);
---
+++ src/Timor.Cms.Service/Attachments/AttachmentService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timor.Cms.Domains.Articles;
+using Timor.Cms.Repository.MongoDb;
+
+namespace Timor.Cms.Service.Attachments
+{
+    public class AttachmentService
+    {
+        private readonly IMongoDbRepository<Attachment> _attachmentRepository;
+        private readonly IMapper _mapper;
+
+        public AttachmentService(IMongoDbRepository<Attachment> attachmentRepository, IMapper mapper)
+        {
+            _attachmentRepository = attachmentRepository;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// 检查附件数据是否存在
+        /// </summary>
+        /// <param name="id">附件Id</param>
+        /// <returns></returns>
+        public async Task<bool> CheckAttachmentExist(ObjectId id)
+        {
+            var attachment = await _attachmentRepository.GetByIdAsync(id);
+            if (attachment == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
