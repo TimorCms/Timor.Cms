4c19dbc695519a94387ed5f437c5df43cfec8e22
YunchenBai <yunchen.bai@thoughtworks.com>
2020-05-06 21:44:06 +0800

    change validate filter to async action filter

--- src/Timor.Cms.Api/Filters/ValidationFilter.cs
+++ src/Timor.Cms.Api/Filters/ValidationFilter.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -7,28 +8,28 @@ using Timor.Cms.Infrastructure.Models;
 
 namespace Timor.Cms.Api.Filters
 {
-    public class ValidationFilter : IActionFilter
+    public class ValidationFilter : IAsyncActionFilter
     {
-        public void OnActionExecuted(ActionExecutedContext context)
-        {
-        }
-
-        public void OnActionExecuting(ActionExecutingContext context)
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (!context.ModelState.IsValid)
             {
                 var errors = new List<ValidationError>();
-                    
+
                 foreach (var error in context.ModelState)
                 {
                     var field = error.Key;
                     var message = error.Value.Errors.FirstOrDefault()?.ErrorMessage;
-                    
-                    errors.Add(new ValidationError(field,message));
+
+                    errors.Add(new ValidationError(field, message));
                 }
 
                 context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
                 context.Result = new JsonResult(new ValidationResult(errors));
+            }
+            else
+            {
+                await next();    
             }
         }
     }
